0d5915fc3de8b7813defb45263fcaac7d9608abf
Вагина Валерия Дмитриевна <vagina.v@ccam.cmc.unn.net>
2019-06-03 14:01:10 +0300

    Practice7l

--- vagina_vd/Practice/Practice7/Vector.h
+++ vagina_vd/Practice/Practice7/Vector.h
@@ -1,65 +1,65 @@
-#ifndef _VECTOR_H_
+﻿#ifndef _VECTOR_H_
 #define _VECTOR_H_
 
 typedef struct Vector
 {
 private:
-	int size;
-	double* elements;
+    int size;
+    double* elements;
 public:
-	Vector(Vector&);
-	Vector(int);
-	~Vector();
-	void Create(double*);
-	void Output();
+    Vector(Vector&);
+    Vector(int);
+    ~Vector();
+    void Create(double*);
+    void Output();
 
-	const Vector& operator=(const Vector&);
-	Vector operator+(const Vector&); //ñëîæåíèå
-	Vector operator+(double);
-	Vector operator-(const Vector&); //âû÷èòàíèå
-	Vector operator-(double);
-	Vector operator*(const Vector&); //âåêòîðíîå ïðîèçâåäåíèå
-	Vector operator*(double); //óìíîæåíèå íà ÷èñëî
-	Vector operator/(double); //äåëåíèå íà ÷èñëî
-	
-	Vector& operator+=(double);
-	Vector operator+=(const Vector&);
-	Vector& operator-=(double);
-	Vector operator-=(const Vector&);
-	Vector& operator*=(double);
+    const Vector& operator=(const Vector&);
+    Vector operator+(const Vector&); //ñëîæåíèå
+    Vector operator+(double);
+    Vector operator-(const Vector&); //âû÷èòàíèå
+    Vector operator-(double);
+    Vector operator*(const Vector&); //âåêòîðíîå ïðîèçâåäåíèå
+    Vector operator*(double); //óìíîæåíèå íà ÷èñëî
+    Vector operator/(double); //äåëåíèå íà ÷èñëî
+    
+    Vector& operator+=(double);
+    Vector operator+=(const Vector&);
+    Vector& operator-=(double);
+    Vector operator-=(const Vector&);
+    Vector& operator*=(double);
 
-	double Lenght() const;
+    double Lenght() const;
 
-	double& operator[](int);
-	const double& operator[](int) const;
+    double& operator[](int);
+    const double& operator[](int) const;
 
-	void* operator new(size_t);
-	void operator delete(void*);
+    void* operator new(size_t);
+    void operator delete(void*);
 
-	friend ostream& operator<<(ostream&, const Vector&);
+    friend ostream& operator<<(ostream&, const Vector&);
 };
 
 class ExceptionSize : exception
 {
 private:
-	const string msg;
+    const string msg;
 public:
-	ExceptionSize(string exc)
-		: msg(exc)
-	{
-	}
-	const char* what() const { return msg.c_str(); }
+    ExceptionSize(string exc)
+        : msg(exc)
+    {
+    }
+    const char* what() const { return msg.c_str(); }
 };
 
 class ExceptionIdx : exception
 {
 private:
-	const string msg;
+    const string msg;
 public:
-	ExceptionIdx(string exc)
-		: msg(exc)
-	{
-	}
-	const char* what() const { return msg.c_str(); }
+    ExceptionIdx(string exc)
+        : msg(exc)
+    {
+    }
+    const char* what() const { return msg.c_str(); }
 };
 #endif
